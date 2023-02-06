8a8022ed672d82482b7e4d78212e8a5b1a4509f6
jaehyeong2 <wogud1514@naver.com>
2023-02-06 11:02:48 +0900

    start

---
+++ src/main/java/jjfactorry/socketto/SockettoApplication.java
@@ -0,0 +1,13 @@
+package jjfactorry.socketto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockettoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockettoApplication.class, args);
+    }
+
+}
